7b8891a7b9a63a3c088ef82d81b04730c2a7eac1
Daniel Wolter <wolter.daniel@gmail.com>
2022-07-28 16:15:48 -0300

    teste elefente 1

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,20 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('recebendo como parâmetro "count" e conferindo o retorno da contagem populacional de 4', () => {
+    const actual = handlerElephants('count');
+    const expected = 4;
+    expect(actual).toBe(expected);
+  });
+  it('recebendo como parâmetro "names" e conferindo o retorno da contem jeferson', () => {
+    const actual = handlerElephants('names');
+    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(actual).toEqual(expected);
+  });
+  it('recebendo como parâmetro "averageAge" e conferindo o retorno é um numero proximo a 10.5', () => {
+    const actual = handlerElephants('averageAge');
+
+    const expected = 10.5;
+    expect(actual).toEqual(expected);
+  });
+});
