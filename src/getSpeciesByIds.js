2343a25e35711c674170142b0a0cebe9e30d91b8
Daniel Wolter <wolter.daniel@gmail.com>
2022-07-29 09:46:28 -0300

    requisito 1 okk

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,17 +1,7 @@
-
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return data.species.filter((animal) => {
-    return ids.find((id) => {
-      return id === animal.id
-    })
-  })
-    
-  }
-
+  return data.species.filter((animal) => ids.find((id) => id === animal.id));
+}
 
 module.exports = getSpeciesByIds;
-
-
-
