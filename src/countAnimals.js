faec6998b6f393388986799eb88f6286d829ca70
Daniel Wolter <wolter.daniel@gmail.com>
2022-07-29 20:59:53 -0300

    countAnimal ok

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,24 +1,29 @@
 const data = require('../data/zoo_data');
 
+function entrada(animal) {
+  if (Object.values(animal).length === 2) {
+    return data.species
+      .filter((especie) => especie.name === Object.values(animal)[0])
+      .map((elem) =>
+        elem.residents.filter((s) => s.sex === Object.values(animal)[1]))
+      .reduce((cont, resultado) => cont + resultado.length, 0);
+  }
+  return data.species
+    .filter((especie) => especie.name === Object.values(animal)[0])
+    .reduce((cont, resultado) => cont + resultado.residents.length, 0);
+}
+
 function countAnimals(obj) {
-  // if (obj === undefined) {
-  //   return data.species.reduce((newobj, elemento) => {
-  //     newobj[elemento.name] = elemento.residents.length;
-  //     return newobj;
-  //   }, {});
-  // }
-  // if (obj.sex === 'female' || obj.sex === 'male') {
-  //   return data.species.reduce((newobj2, elemento2) => {
-  //     if (obj.specie === elemento2.name) {
-  //       console.log(obj.specie, elemento2.name, elemento2.residents.length);
-  //       newobj2[elemento2.name] = elemento2.residents.length;
-  //       return newobj2;
-  //     }
-  //   }, {});
-  // }
+  const newObj = {};
+  if (obj === undefined) {
+    data.species.forEach((item) => {
+      newObj[item.name] = item.residents.length;
+    });
+    return newObj;
+  }
+  return entrada(obj);
 }
 
 module.exports = countAnimals;
-// return data.species.filter((animal) => ids.find((id) => id === animal.id));
 
-// console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
+// countAnimals({ specie: 'penguins' })
