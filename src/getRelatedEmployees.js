59539f0aa5e01c8218393290ae60a18ebe39292a
Daniel Wolter <wolter.daniel@gmail.com>
2022-07-31 10:16:11 -0300

    getRelatedEmployees ok

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  let manager = false;
+  employees.forEach((employee) => {
+    if (employee.managers.some((managerId) => managerId === id)) {
+      manager = true;
+    }
+  });
+  return manager;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees.reduce((acc, employee) => {
+    if (employee.managers.some((id) => id === managerId)) {
+      acc.push(`${employee.firstName} ${employee.lastName}`);
+    }
+    return acc;
+  }, []);
 }
 
 module.exports = { isManager, getRelatedEmployees };
