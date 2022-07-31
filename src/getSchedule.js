9a2f3aa93b98ee36a6a3afd8c54d3fbb551fc69f
Daniel Wolter <wolter.daniel@gmail.com>
2022-07-31 18:44:47 -0300

    getSchedule ok

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,53 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const { species, hours } = data;
+
+function getAnimal(animal) {
+  const specie = species.find((pet) => pet.name === animal);
+  if (!specie) return false;
+  return specie;
 }
 
+function isDayOfWeek(day) {
+  const DAYS_OF_WEEK = Object.keys(hours);
+  if (DAYS_OF_WEEK.includes(day)) return true;
+  return false;
+}
+
+function getAnimalByDay(day) {
+  return species
+    .filter((specie) => specie.availability.includes(day))
+    .map((specie) => specie.name);
+}
+
+function getFullSchedule() {
+  const DAYS_OF_WEEK = Object.keys(hours);
+  return DAYS_OF_WEEK.reduce((acc, day) => {
+    if (day === 'Monday') {
+      acc[day] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    } else {
+      acc[day] = {
+        officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+        exhibition: getAnimalByDay(day),
+      };
+    }
+    return acc;
+  }, {});
+}
+
+function getSchedule(scheduleTarget) {
+  const specie = getAnimal(scheduleTarget);
+  if (specie) {
+    return specie.availability;
+  }
+
+  if (isDayOfWeek(scheduleTarget)) {
+    return { [scheduleTarget]: getFullSchedule()[scheduleTarget] };
+  }
+
+  return getFullSchedule();
+}
 module.exports = getSchedule;
